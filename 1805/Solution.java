22dc8b7ab3d8f73e303697c5a69539858cbb2cb2
Ming Ki Chong <unexpected.exception@gmail.com>
2021-06-03 22:17:22 -0700

    https://leetcode.com/problems/number-of-different-integers-in-a-string/

---
+++ 1805/Solution.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int numDifferentIntegers(String word) {
+        StringBuilder sb = new StringBuilder();
+        for (char a : word.toCharArray()) {
+            sb.append((a >= '0' && a <= '9') ? a : " ");
+        }
+        Set<String> set = new HashSet<>();
+        for (String numStr : sb.toString().split(" ")) {
+            if (!numStr.equals("")) {
+                set.add(trimLeadingZeroes(numStr));
+            }
+        }
+        return set.size();
+    }
+
+    private String trimLeadingZeroes(String numStr) {
+        int i = 0;
+        while (i < numStr.length() - 1 && numStr.charAt(i) == '0') {
+            i++;
+        }
+        return numStr.substring(i, numStr.length());
+    }
+
+    public static void main(String args[]) {
+        Solution s = new Solution();
+        System.out.println(s.numDifferentIntegers("a123bc34d8ef34")); // 3
+        System.out.println(s.numDifferentIntegers("leet1234code234")); // 2
+        System.out.println(s.numDifferentIntegers("a1b01c001")); // 1
+        System.out.println(s.numDifferentIntegers("abcdefg")); // 0
+        System.out.println(s.numDifferentIntegers("1234567890987654321")); // 0
+    }
+}
